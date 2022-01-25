76aa73759faf95a1d36a9eac6899cfc5911135f1
David Opoku <71300793+dopoku12@users.noreply.github.com>
2022-01-25 16:38:16 -0500

    time added and weaherAPI reformated

--- assests/script.js
+++ assests/script.js
@@ -2,42 +2,50 @@ const savedSearch = document.querySelector('.savedSearch')
 const search = document.getElementById('search')
 const searchBtn = document.getElementById('searchBtn')
 const clearBtn = document.getElementById('clearBtn')
+const nameNDtime = document.getElementById("nameNDtime")
 const temp = document.getElementById("temp")
 const wind = document.getElementById("wind")
 const humidity = document.getElementById("humidity")
 const uvIndex = document.getElementById("uvIndex")
 const temp1 = document.getElementById('temp1')
 
-
-const save = function () {
-
-    localStorage.setItem(`${search.value}`, JSON.stringify(weatherApi))
-    console.log(localStorage);
-    const liTag = document.createElement("li")
-    savedSearch.appendChild(liTag)
-    liTag.textContent = search.value
-
-
+let searchArr = []
+function save(searchVal) {
+    searchVal.forEach(searchHis => {
+        searchHis.push(searchArr)
+        localStorage.setItem('Search History', JSON.stringify(searchHis))
+        console.log(localStorage);
+    });
 }
 
 
+function load() {
+    let history = JSON.parse(localStorage.getItem('Search History'));
+    console.log(history)
+    history.forEach((prevSearch) => {
+        const liTag = document.createElement("li")
+        liTag.innerText = prevSearch
+        savedSearch.appendChild(liTag)
+    });
 
-searchBtn.addEventListener('click', function () {
-    weatherApi(search.value)
-    save()
+}
+window.onload = load()
+
+const initialize = () => {
+    let searchVal = search.value.trim()
+    weatherApi(searchVal)
+    save(searchVal)
+    load()
+}
+
+searchBtn.addEventListener('click', e => {
+    // e.preventDefault()
+    initialize()
+
 })
 
-search.addEventListener('keydown', function (e) {
-    console.log(e);
-    e.keyCode === 13 ? weatherApi(search.value) & save() : console.log('wrong key');
 
-})
-
-
-
-
-
-
+search.addEventListener('keydown', e => { if (e.keyCode === 13) initialize(); })
 
 
 clearBtn.addEventListener('click', function (e) {
@@ -52,23 +60,22 @@ async function weatherApi(cityName) {
     let data = await response.json()
     const latitude = data.coord.lat
     const longitude = data.coord.lon
+    //second Api    
     let responseTwo = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&exclude={part}&appid=68ef11f1d0efb99f09ab6da7a559dd58`)
     let dataTwo = await responseTwo.json()
     console.log('weatherApi :', dataTwo);
-
-    data.name
+    //content
+    console.log(dataTwo.timezone)
+    const date = new Date().toLocaleString('en-US', { timeZone: `${dataTwo.timezone}` });
+    console.log(date);
+    let str = `${cityName.toUpperCase()}  (${date}) `
+    console.log(str);
     const current = dataTwo.current
-    temp.textContent = ` Temp: ${current.temp}°F`
-    wind.textContent = ` Wind speed: ${current.wind_speed}MPH`
-    humidity.textContent = ` Humidity: ${current.humidity}%`
-    uvIndex.textContent = ` UV index: ${current.uvi}`
-
+    temp.innerText = ` Temp: ${current.temp}°F`
+    wind.innerText = ` Wind speed: ${current.wind_speed}MPH`
+    humidity.innerText = ` Humidity: ${current.humidity}%`
+    uvIndex.innerText = ` UV index: ${current.uvi}`
+    nameNDtime.innerText = str
 }
-
-
-
-
-
-
 
 
