be1eee44fc8f152c98fd6c34d8641371523c1344
David Opoku <71300793+dopoku12@users.noreply.github.com>
2022-12-03 02:36:18 -0500

    fixed 5 day forecast display

--- assests/script.js
+++ assests/script.js
@@ -14,29 +14,11 @@ const mainTemp = document.getElementById(`mainTemp`);
 const mainWind = document.getElementById(`mainWind`);
 const mainHumidity = document.getElementById(`mainHumidity`);
 const mainUvIndex = document.getElementById(`mainUvIndex`);
-let num = 0
-//INITIALIZE SEARCH AND SAVE// 
+let num = 0;
+//INITIALIZE SEARCH// 
 const initialize = () => {
-    let searchValue = search.value.trim()
-    usrInput(searchValue)
-    save(searchValue)
-    // window.onload = load()
-}
-let inputArr = []
-//SAVE SEARCH VALUE//
-const save = (input) => {
-    let outputArr = [input]
-    outputArr.map(history => {
-        localStorage.setItem('Search History', JSON.stringify(history));
-        let parsedVal = JSON.parse(localStorage.getItem('Search History'));
-        console.log('passed', parsedVal)
-        const liTag = document.createElement("li")
-        liTag.innerText = history
-        savedSearch.appendChild(liTag)
-    })
-    // outputArr.forEach((history) => {
-    //     console.log("history", history);
-    // })
+    let searchValue = search.value.trim();
+    usrInput(searchValue);
 }
 searchBtn.addEventListener('click', initialize);
 search.addEventListener('keydown', e => { if (e.keyCode === 13) initialize(); });
@@ -45,16 +27,14 @@ search.addEventListener('keydown', e => { if (e.keyCode === 13) initialize(); })
 async function usrInput(inputName) {
     let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputName}&appid=68ef11f1d0efb99f09ab6da7a559dd58`)
     console.log("res:", res);
-    let data = await res.json()
+    let data = await res.json();
     console.log('secondApi:', data);
     //if (404/NOT FOUND) DOES NOT OCCUR PROCEED//
     if (res.ok) {
-        const searchName = data.name
-        console.log('name:', searchName)
+        const searchName = data.name;
         let latitude = data.coord.lat;
         let longitude = data.coord.lon;
-        display(latitude, longitude, searchName)
-        console.log('long:', longitude, 'lat:', latitude)
+        display(latitude, longitude, searchName);
     }
 }
 
@@ -63,7 +43,7 @@ if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition((position) => {
         let latitude = position.coords.latitude;
         let longitude = position.coords.longitude;
-        display(latitude, longitude)
+        display(latitude, longitude);
     });
 }
 
@@ -71,14 +51,10 @@ if ("geolocation" in navigator) {
 async function display(latitude, longitude, searchName = null) {
     // THIRD API WEATHER DATA //
     let resThree = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&exclude={part}&appid=68ef11f1d0efb99f09ab6da7a559dd58`)
-    console.log('long2:', longitude, 'lat2:', latitude)
-    let dataThree = await resThree.json()
-    console.log('weatherApi :', dataThree);
-    console.log(dataThree.timezone)
+    let dataThree = await resThree.json();
     //FOURTH API USED REVERSE GEOCODING//
     let resFour = await fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=5&appid=68ef11f1d0efb99f09ab6da7a559dd58`)
-    let dataFour = await resFour.json()
-    console.log(dataFour)
+    let dataFour = await resFour.json();
     const outputName = dataFour[0].name;
     //displaying forecast on .present-forecast//
     const current = dataThree.current;
@@ -91,40 +67,34 @@ async function display(latitude, longitude, searchName = null) {
      Humidity`;
     //uvIndex text content and backgroundColor//
     mainUvIndex.innerText = `UV index: ${current.uvi}`;
-    current.uvi >= 6 ? mainUvIndex.style.backgroundColor = 'red' : mainUvIndex.style.backgroundColor = 'green'
+
+    current.uvi >= 6 ? mainUvIndex.style.backgroundColor = 'red'
+        : mainUvIndex.style.backgroundColor = 'green';
 
     //displaying time/date,cityName on .present-forecast//
-    const dateObj = new Date()
+    const dateObj = new Date();
     const date = dateObj.toLocaleString('en-US', { timeZone: `${dataThree.timezone}` });
-    // const month = dateObj.toLocaleString('en-us', { month: 'long' });
-    // const today = dateObj.toLocaleString("default", { weekday: "long" });
     const dateArr = date.split(',');
-
-    console.log(dateArr);
     const hourNMin = dateArr[1].split(':');
     const meridian = dateArr[1].split(':')[2].split(" ");
     name.innerText = !searchName ? outputName : searchName;
     time.innerText = `${hourNMin[0]}:${hourNMin[1]} ${meridian[1]}`;
-    globalDate.innerHTML = `${dateArr[0]}`
-    //displaying forecast on .forecast
-    let dailyArr = dataThree.daily
-
-    dailyArr.forEach(function (day) {
+    globalDate.innerHTML = `${dateArr[0]}`;
+    //displaying forecast on .forecast//
+    let dailyArr = dataThree.daily;
+    console.log('dayWeather:', dailyArr, num);
+    dailyArr.forEach((day) => {
         //DAILY FORECAST//
         if (num <= 4) {
             let temp = document.getElementById(`temp${num}`);
             let wind = document.getElementById(`wind${num}`);
             let humidity = document.getElementById(`humidity${num}`);
             let sideIcon = document.getElementById(`icon${num}`);
+            console.log('dayWeather:', day.temp.day, num);
             temp.innerText = `${day.temp.day}°F`;
             sideIcon.innerHTML = `<img src= http://openweathermap.org/img/w/${day.weather[0].icon}.png></img>`;
         };
-        num++
+        num++;
     });
-}
-
-//clear
-clearBtn.addEventListener('click', function (e) {
-    e.preventDefault()
-    localStorage.clear()
-})
+    num = 0;
+};
